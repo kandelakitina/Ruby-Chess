bda835f534c5cb04aaef783234204e51cd965112
boticelli <kandelakitina@gmail.com>
2025-08-20 22:17:45 +0300

    Update board_spec to test fill_board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,28 +11,46 @@ RSpec.describe Board, type: :model do
       expect(board.grid.all? { |row| row.size == 8 }).to be true
     end
 
-    it 'creates distinct objects for each grid cell' do
-      flattened = board.grid.flatten
-
-      # Only compare EmptyCell objects to ensure they are distinct
-      empty_cells = flattened.select { |cell| cell.is_a?(EmptyCell) }
-
-      # Expect all EmptyCell instances to be unique objects
-      empty_cells.combination(2).each do |cell1, cell2|
-        expect(cell1).not_to be(cell2)
-      end
+    it 'creates distinct EmptyCell instances (not the same object)' do
+      first = board.grid[2][0] # empty row
+      second = board.grid[2][1]
+      expect(first).not_to be(second)
     end
 
-    it 'creates distinct EmptyCell instances (not the same object)' do
-      first = board.grid[0][0]
-      second = board.grid[0][1]
-      expect(first).not_to be(second)
+    it 'places 16 white pieces and 16 black pieces' do
+      flattened = board.grid.flatten
+      white_pieces = flattened.count { |cell| cell.is_a?(Piece) && cell.color == :white }
+      black_pieces = flattened.count { |cell| cell.is_a?(Piece) && cell.color == :black }
+
+      expect(white_pieces).to eq(16)
+      expect(black_pieces).to eq(16)
+    end
+
+    it 'places pawns in the correct rows' do
+      expect(board.grid[1].all? { |cell| cell.is_a?(Pawn) && cell.color == :white }).to be true
+      expect(board.grid[6].all? { |cell| cell.is_a?(Pawn) && cell.color == :black }).to be true
+    end
+
+    it 'places rooks, knights, bishops, queen, and king in the correct positions' do
+      # White back rank
+      expect(board.grid[0][0]).to be_a(Rook).and have_attributes(color: :white)
+      expect(board.grid[0][1]).to be_a(Knight).and have_attributes(color: :white)
+      expect(board.grid[0][2]).to be_a(Bishop).and have_attributes(color: :white)
+      expect(board.grid[0][3]).to be_a(Queen).and have_attributes(color: :white)
+      expect(board.grid[0][4]).to be_a(King).and have_attributes(color: :white)
+
+      # Black back rank
+      expect(board.grid[7][0]).to be_a(Rook).and have_attributes(color: :black)
+      expect(board.grid[7][1]).to be_a(Knight).and have_attributes(color: :black)
+      expect(board.grid[7][2]).to be_a(Bishop).and have_attributes(color: :black)
+      expect(board.grid[7][3]).to be_a(Queen).and have_attributes(color: :black)
+      expect(board.grid[7][4]).to be_a(King).and have_attributes(color: :black)
     end
   end
 
   describe '#empty_at?' do
     it 'returns true for an empty cell within bounds' do
-      expect(board.empty_at?([0, 0])).to be true
+      expect(board.empty_at?([2, 0])).to be true
     end
 
     it 'returns false for a cell occupied by a piece' do
