48a3de0721991da8a38655e69ab48a1e791b9b96
boticelli <kandelakitina@gmail.com>
2025-08-20 22:12:34 +0300

    Add all pieces (without moves)

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,11 @@
 require 'colorize'
 require_relative 'empty_cell'
 require_relative 'pieces/pawn'
+require_relative 'pieces/knight'
+require_relative 'pieces/bishop'
+require_relative 'pieces/rook'
+require_relative 'pieces/queen'
+require_relative 'pieces/king'
 
 class Board
   attr_accessor :grid
---
+++ lib/pieces/queen.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative 'piece'
+
+class Queen < Piece
+  def symbol
+    'q'
+  end
+
+  # def possible_moves(board, row, col)
+  #   moves = []
+
+  #   direction = color == :white ? 1 : -1
+  #   start_row = color == :white ? 1 : 6
+
+  #   # 1. Forward one
+  #   one_ahead = [row + direction, col]
+  #   moves << one_ahead if board.empty_at?(one_ahead)
+
+  #   # 2. Forward two from starting row
+  #   two_ahead = [row + (2 * direction), col]
+  #   moves << two_ahead if row == start_row && board.empty_at?(one_ahead) && board.empty_at?(two_ahead)
+
+  #   # 3. Diagonal captures
+  #   [[row + direction, col + 1], [row + direction, col - 1]].each do |r, c|
+  #     moves << [r, c] if board.enemy_at?([r, c], color)
+  #   end
+
+  #   moves
+  # end
+end
